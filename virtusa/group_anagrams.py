dfab8adb84a9506431ec0977f30a6e7806771f1f
Sivaji Chowdary <60888653+Sivaji-Marripudi@users.noreply.github.com>
2021-02-03 11:58:01 +0530

    First commit

--- virtusa/group_anagrams.py
+++ virtusa/group_anagrams.py
@@ -1,8 +1,8 @@
-words = ['lamp','plam','eat','ate','i','i']
+words = ['lamp','plam','eat','ate','i','i','siva']
 m_words = [''.join(sorted(i)) for i in words]
 a = list(zip(words,m_words))
 d = {}
 for i in a:
     d.setdefault(i[1],[]).append(i[0])
-res = [j for j in d.values()]
+res = [j for j in d.values() if len(j) > 1]
 print(res)
